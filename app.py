009c0c0020cece5a32e9e2cc7eede65fa6663832
Simbamon <sbhan9511@gmail.com>
2021-04-28 21:49:13 +0900

    kakao message

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, json
 from flask_mail import Mail, Message
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -33,6 +35,55 @@ def index():
 
     mail.send(msg)
     return "Success" 
+
+@app.route('/gettoken')
+def token():
+    url = "https://kauth.kakao.com/oauth/token"
+
+    token_data = {
+        "grant_type" : "authorization_code",
+        "client_id" : "",
+        "redirect_uri" : "http://127.0.0.1:5000/sendkakao",
+        "code" : ""
+        
+    }
+    response = requests.post(url, data=token_data)
+
+    tokens = response.json()
+
+    print(tokens)
+
+    with open("kakao_token.json", "w") as fp:
+        json.dump(tokens, fp)
+
+    return "successfully generated token"
+
+@app.route('/sendkakao')
+def send_k():
+
+    with open("kakao_token.json", "r") as fp:
+        access_token = json.load(fp)
+
+    print(access_token['access_token'])
+    headers = {'Authorization': 'Bearer ' + access_token['access_token']}
+    data = {
+    "template_object" : json.dumps({ "object_type" : "text",
+                                     "text" : "Hello, world!",
+                                     "link" : {
+                                                 "web_url" : "www.naver.com"
+                                              }
+        })
+    }
+
+    response = requests.post("https://kapi.kakao.com/v2/api/talk/memo/default/send", headers=headers, data=data)
+    
+    print(response.status_code)
+    if response.json().get('result_code') == 0:
+        print('Success')
+    else:
+        print('Error: ' + str(response.json()))
+
+    return "sucessfully sent message"
 
 if __name__ == '__main__':
     app.run()
